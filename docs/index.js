a955408199d281bf20b14c8f0b6619f3f71968c2
Lukas Batema <lukasbatema@gmail.com>
2021-02-20 20:04:23 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 function welcomeToSite() {
   var usersName = prompt("Enter your name!");
   document.getElementById("greetings").innerHTML += 
-           "Welcome, " + usersName + ", glad to see you are checking out my site!" + "<br>";
+           "Welcome, " + usersName + "! Glad to see you are checking out my site! It means a lot to me, thanks!";
 }
