ab2939b1ed923258c4bca5dcfd479d3f85a93d91
Lukas Batema <lukasbatema@gmail.com>
2021-02-27 17:30:01 -0500

    Update themeChooser.js

--- docs/js/stylesheets/themeChooser.js
+++ docs/js/stylesheets/themeChooser.js
@@ -2,7 +2,7 @@ function changeTheme() {
   var selTheme = document.getElementById("selTheme");
   var theme = selTheme.value;
         
-  if (selTheme.value == "Light") {
+  if (theme == Light) {
     document.body.style.backgroundColor = white;
     document.header.style.backgroundColor = lightgray;
     document.header.style.color = rgb(112, 103, 103);
@@ -11,15 +11,15 @@ function changeTheme() {
     document.left.style.backgroundColor = rgb(97, 97, 100);
     document.right.style.backgroundColor = rgb(61, 61, 63);
     document.topnav.ul.style.backgroundColor = rgb(139, 137, 133);
-    document.li.a:hover:not.active.style.backgroundColor = rgb(34, 34, 34);
-    document.li.a:hover:not.active.style.color = rgb(115, 116, 115);
-    document.dropdown:hover:not.active.dropbtn.style.backgroundColor = rgb(34, 34, 34);
-    document.dropdown:hover:not.active.dropbtn.style.color = rgb(115, 116, 115);
-    document.topnav.dropdown-content.a:hover.style.backgroundColor = rgb(99, 97, 99);
-    document.topnav.dropdown-content.a:hover.style.color = rgb(37, 37, 37);
+    document.li.a.hover.not.active.style.backgroundColor = rgb(34, 34, 34);
+    document.li.a.hover.not.active.style.color = rgb(115, 116, 115);
+    document.dropdown.hover.not.active.dropbtn.style.backgroundColor = rgb(34, 34, 34);
+    document.dropdown.hover.not.active.dropbtn.style.color = rgb(115, 116, 115);
+    document.topnav.dropdown.content.a.hover.style.backgroundColor = rgb(99, 97, 99);
+    document.topnav.dropdown.content.a.hover.style.color = rgb(37, 37, 37);
     document.topnav.active.style.backgroundColor = rgb(37, 37, 37);
-    document.topnav.active.style.backgroundColor = rgb(99, 99 ,99);
-  } else if (selTheme.value == "Dark") {
+    document.topnav.active.style.color = rgb(99, 99 ,99);
+  } else if (theme == Dark) {
     document.body.style.backgroundColor = rgb(80, 77, 77);
     document.header.style.backgroundColor = black;
     document.header.style.color = rgb(112, 103, 103);
@@ -28,13 +28,13 @@ function changeTheme() {
     document.left.style.backgroundColor = rgb(97, 97, 100);
     document.right.style.backgroundColor = rgb(61, 61, 63);
     document.topnav.ul.style.backgroundColor = rgb(139, 137, 133);
-    document.li.a:hover:not.active.style.backgroundColor = rgb(34, 34, 34);
-    document.li.a:hover:not.active.style.color = rgb(115, 116, 115);
-    document.dropdown:hover:not.active.dropbtn.style.backgroundColor = rgb(34, 34, 34);
-    document.dropdown:hover:not.active.dropbtn.style.color = rgb(115, 116, 115);
-    document.topnav.dropdown-content.a:hover.style.backgroundColor = rgb(99, 97, 99);
-    document.topnav.dropdown-content.a:hover.style.color = rgb(37, 37, 37);
+    document.li.a.hover.not.active.style.backgroundColor = rgb(34, 34, 34);
+    document.li.a.hover.not.active.style.color = rgb(115, 116, 115);
+    document.dropdown.hover.not.active.dropbtn.style.backgroundColor = rgb(34, 34, 34);
+    document.dropdown.hover.not.active.dropbtn.style.color = rgb(115, 116, 115);
+    document.topnav.dropdown.content.a.hover.style.backgroundColor = rgb(99, 97, 99);
+    document.topnav.dropdown.content.a.hover.style.color = rgb(37, 37, 37);
     document.topnav.active.style.backgroundColor = rgb(37, 37, 37);
-    document.topnav.active.style.backgroundColor = rgb(99, 99 ,99);
+    document.topnav.active.style.color = rgb(99, 99 ,99);
   }
 }
