4600e366973d3dd0d087dabeef753804af53f9a7
Lukas Batema <lukasbatema@gmail.com>
2022-01-16 18:55:52 -0500

    Fix

---
+++ src/javascript/db/index.js
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+export function query(text, params, callback) {
+  return pool.query(text, params, callback);
+}
