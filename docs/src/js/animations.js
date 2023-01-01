c51cb21961e34c1fcdc7cb929c007ab23e8d1019
BatemaDevelopment <lukasbatema@mg.batemadevelopment.com>
2023-01-01 15:06:34 -0500

    Pending changes exported from your codespace (#49)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ docs/src/js/animations.js
@@ -0,0 +1,16 @@
+function hoverNavLink() {
+  const hover = document.querySelector("nav#navbar a:hover").id;
+
+  document.getElementById(hover).style.animation = "navBarHover ease-in 500ms";
+
+  document.getElementById(hover).style.color = "#c1c7ca";
+}
+
+function un_hoverNavLink() {
+  const un_hover = document.querySelector("nav#navbar a:not(:hover)").id;
+
+  document.getElementById(un_hover).style.animation =
+    "navBarUn_hover ease-out 500ms";
+
+  document.getElementById(un_hover).style.color = "#ffffff";
+}
